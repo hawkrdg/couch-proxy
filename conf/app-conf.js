83966b35a3043471de81a0564b5eaa4ba47d6e44
cliff <cliff@hawkrdg.com>
2022-03-22 09:38:28 -0500

    Update app-conf.js

--- conf/app-conf.js
+++ conf/app-conf.js
@@ -1,24 +1,23 @@
 conf = {
   network: {
-    server_url: '127.0.0.1',
-    server_port: 80,
+    server_url: 'my.server.url',
     secure_server_port: 443,
-    couchdb_url: '192.168.1.25',
+    couchdb_url: '127.0.0.1',
     couchdb_port: 5984,
     server_cert_file: './certs/server.crt',
     server_key_file: './certs/server.key'
   },
 
   logs: {
-    level: 'info',
-    rotateFrequency: '1d',
-    maxLogFiles: '60d',
+    level: 'info',              // log 'info' or worse...
+    rotateFrequency: '1d',      // set to rotate daily...
+    maxLogFiles: '60d',         // keep sixty days of logs...
     datePattern: 'yyyy-MM-dd',
     appLogFolder: 'appLogs',
     couchLogFolder: 'couchDBLogs',
   },
 
-  run_as_service: true
+  run_as_service: true          // set this to false for command-line, true for systemd
 }
 
 module.exports = conf;
